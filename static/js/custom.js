bbb46caf28bb2673de9ea47ebef63a30d61e8a54
freewalker1985 <freewalker1985@126.com>
2019-03-21 16:30:39 +0800

    服务器ip选择终于搞定了

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,15 @@
 $('#myModal').on('show.bs.modal', function (e) {
     let link = $(e.relatedTarget);
-    $(this).find(".modal-body").load(link.attr("href"));
+     $(this).find(".modal-body").load(link.attr("href"));
 });
 
 
-$("#right_move").click(function () {
-    console.log($("#server_list option:selected"));
-    $("#server_list_execute").append($("#server_list option:selected"));
+
+$().ready(function() {
+    $("#right_move").click(function () {
+        $("#server_list_execute").append($("#server_list option:selected"));
+    });
 });
+
+
 
